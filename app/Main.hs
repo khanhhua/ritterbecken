7b305ac1b36a5b9b1bb2489cd47abbde58d3ead4
Khanh Hua <giakhanh2487@gmail.com>
2022-07-27 23:31:22 +0200

    Move the Ritter

--- app/Main.hs
+++ app/Main.hs
@@ -2,36 +2,68 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Control.Monad
-    (join, forever)
+    (forever, join)
+import Control.Monad.State.Lazy
 import Data.List
 import Glyph
 import Model
     (Cell (..), Player (..))
+import Prelude hiding (Left, Right)
+import System.IO (hFlush, stdout)
 
 data Action
-  = Up Int
-  | Down Int
-  | Left Int
-  | Right Int
+  = Up
+  | Down
+  | Left
+  | Right
   | Attack
 
 
 type Board = [[Cell]]
+type Location = (Int, Int)
+
+data PlayerInfo = PlayerInfo
+  { player :: Player
+  , location :: Location
+  }
 
 data GameState
   = GameState
       { board   :: Board
-      , players :: (Player, Player)
+      , players :: (PlayerInfo, PlayerInfo)
       }
 
-newtype Game
-  = Game { update :: Action -> GameState -> IO (String, GameState) }
+render :: GameState -> String
+render GameState { board } =
+  show $ table board
 
-putPlayer :: GameState -> (Int, Int) -> Player -> GameState
-putPlayer GameState { board, players } (i, j) player =
-    GameState updatedBoard players
+swap :: (a, a) -> (a, a)
+swap (p1, p2) = (p2, p1)
+
+fstUpdate :: (a, a) -> a -> (a, a)
+fstUpdate (p1, p2) newP = (newP, p2)
+
+defaultBoard = [ [Water, Water, Water, Water, Water]
+               , [Occupied Ritter, Float, Float, Float, Float]
+               , [Float, Float, Float, Float, Float]
+               , [Float, Float, Float, Float, Float]
+               , [Float, Float, Float, Float, Float]
+               , [Float, Float, Float, Float, Occupied Wikinger]
+               , [Water, Water, Water, Water, Water]
+               ]
+
+type Game a = State GameState a
+
+movePlayer :: (Int, Int) -> Player -> Game ()
+movePlayer (i, j) player = do
+  GameState { board, players } <- get
+  let
+    updatedBoard = updateBoard board
+    updatedPlayers = fstUpdate players (PlayerInfo player (i, j))
+  put $ GameState updatedBoard updatedPlayers
+
   where
-    updatedBoard = map f (withIndex board)
+    updateBoard board = map f (withIndex board)
     f (i_, row) =
       map (g i_) (withIndex row)
 
@@ -44,32 +76,54 @@ putPlayer GameState { board, players } (i, j) player =
         | otherwise = Occupied p
     withIndex xs = zip (findIndices (const True) xs) xs
 
-getPlayer :: GameState -> Player
-getPlayer gameState = undefined
+getActivePlayer :: Game PlayerInfo
+getActivePlayer = gets (fst . players)
 
-rotatePlayer :: GameState -> (Player, GameState)
-rotatePlayer gameState = undefined
+rotatePlayer :: Game ()
+rotatePlayer = do
+  swappedPlayers <- swap <$> gets players
+  modify (\GameState { board } -> GameState board swappedPlayers)
 
-render :: GameState -> String
-render GameState { board } =
-  show $ table board
-
+applyAction :: Action -> Game GameState
+applyAction action = do
+  PlayerInfo { player, location } <- getActivePlayer
+  let
+    (i, j) = location
+    updatedLocation = case action of
+      Up -> (i - 1, j)
+      Down -> (i + 1, j)
+      Left -> (i, j - 1)
+      Right -> (i, j + 1)
+      _ -> location
+      
+  movePlayer updatedLocation Ritter
+  get
 
 main :: IO ()
-main =
-  let
-    board = [ [Water, Water, Water, Water, Water]
-            , [Occupied Ritter, Float, Float, Float, Float]
-            , [Float, Float, Float, Float, Float]
-            , [Float, Float, Float, Float, Float]
-            , [Float, Float, Float, Float, Float]
-            , [Float, Float, Float, Float, Occupied Wikinger]
-            , [Water, Water, Water, Water, Water]
-            ]
-    gameState = GameState board (Ritter, Wikinger)
-  in forever $ do
-    putStrLn $ render gameState
+main = forever $ do
+  go initialGameState
+  where
+    initialGameState = GameState defaultBoard
+        ( PlayerInfo Ritter (1, 0)
+        , PlayerInfo Wikinger (5, 4)
+        )
 
-    _ <- getLine
-    putStrLn . render $ putPlayer gameState (2, 2) Ritter
-    getLine
+    go :: GameState -> IO ()
+    go gameState = do
+      putStrLn $ render gameState
+      putStr "Command (A,S,W,D): "
+      hFlush stdout
+      cmd <- getLine
+      let
+        -- TODO Parse series of commands like
+        -- "DDS" -> Right Right Down and exec the applyAction accordingly
+        action = case cmd of
+          "D" -> Right
+          "A" -> Left
+          "W" -> Up
+          "S" -> Down
+          _ -> undefined
+
+        updatedGameState = execState (applyAction action) gameState
+
+      go updatedGameState
