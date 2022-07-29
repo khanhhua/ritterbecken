7ac5f1d70f9abdb6d0fac78d0b89baa4c9a0096f
Khanh Hua <giakhanh2487@gmail.com>
2022-07-29 02:02:19 +0200

    Dice rolling logic

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,12 @@ import Data.List
 import Glyph
 import Model
     (Cell (..), Player (..))
+import Dice ( roll )
 import Prelude hiding (Left, Right)
 import System.IO (hFlush, stdout)
+import System.Random (StdGen, newStdGen)
+import Data.Maybe (isJust, fromJust)
+
 
 data Action
   = Up
@@ -26,7 +30,14 @@ type Location = (Int, Int)
 data PlayerInfo = PlayerInfo
   { player :: Player
   , location :: Location
+  , energy :: Int
   }
+
+instance Show PlayerInfo where 
+  show (PlayerInfo player location energy) =
+    "Player: " <> show player
+      <> " at " <> show location
+      <> " energy " <> show energy
 
 data GameState
   = GameState
@@ -35,8 +46,12 @@ data GameState
       }
 
 render :: GameState -> String
-render GameState { board } =
-  show $ table board
+render GameState { board, players } =
+  let
+    t = show $ table board
+    p = show $ fst players
+  in t <> "\n" <> p
+  
 
 swap :: (a, a) -> (a, a)
 swap (p1, p2) = (p2, p1)
@@ -60,7 +75,10 @@ movePlayer (i, j) player = do
   GameState { board, players } <- get
   let
     updatedBoard = updateBoard board
-    updatedPlayers = fstUpdate players (PlayerInfo player (i, j))
+    PlayerInfo _ (i_, j_) energy = fst players
+    delta = abs (i_ - i) + abs (j_ - j)
+    updatedPlayers = fstUpdate players (PlayerInfo player (i, j) (energy - delta))
+
   put $ GameState updatedBoard updatedPlayers
 
   where
@@ -97,21 +115,41 @@ applyAction action = do
       Right -> (i, j + 1)
       _ -> location
       
-  movePlayer updatedLocation Ritter
+  movePlayer updatedLocation player
   get
 
+grantPlayerEnergy :: Int -> Game ()
+grantPlayerEnergy energy = do
+  gameState <- get
+  activePlayer <- getActivePlayer
+  let
+    GameState { players } = gameState
+    updatedPlayers = fstUpdate players $ activePlayer { energy = energy }
+  put gameState { players = updatedPlayers }
+
 main :: IO ()
-main =
-  go initialGameState
+main = do
+  g <- newStdGen
+  go initialGameState g
   where
     initialGameState = GameState defaultBoard
-        ( PlayerInfo Ritter (1, 0)
-        , PlayerInfo Wikinger (5, 4)
+        ( PlayerInfo Ritter (1, 0) 0
+        , PlayerInfo Wikinger (5, 4) 0
         )
 
-    go :: GameState -> IO ()
-    go gameState = do
+    go :: GameState -> StdGen -> IO ()
+    go gameState g = do
       putStrLn $ render gameState
+      let
+        (m1, dice, nextG) = if (energy . fst . players) gameState == 0
+          then
+            let
+              (dice, nextG) = roll g
+            in (execState (grantPlayerEnergy dice) gameState, Just dice, nextG)
+          else (gameState, Nothing, g)
+      when (isJust dice) $ do
+        putStrLn $ "System has rolled " <> show (fromJust dice)
+
       putStr "Command (A,S,W,D): "
       hFlush stdout
       cmd <- getLine
@@ -122,7 +160,10 @@ main =
           'W' -> Up
           'S' -> Down
           _ -> undefined) <$> cmd
-        m = traverse applyAction actions
-        updatedGameState = execState m gameState
+        applyActionMonad = traverse applyAction actions
+        m2 = execState applyActionMonad m1
+        m3 = if (energy . fst . players) m2 == 0
+          then execState rotatePlayer m2
+          else m2
 
-      go updatedGameState
+      go m3 nextG
